360776afbf889c1a1affcfec627f56c07d64fef1
Anton Bessolicyn <Anton.Bessolicyn@danfoss.com>
2018-09-17 11:16:16 +0300

    Add project files.

---
+++ TechApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TechApplication.Startup))]
+namespace TechApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
