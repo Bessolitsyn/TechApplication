96ce0b683bb320ceb7c3674df061312c33198142
Anton Bessolicyn <Anton.Bessolicyn@danfoss.com>
2019-02-11 17:36:57 +0300

    #изсенения

---
+++ TechApplication/Scripts/SiteScript.js
@@ -0,0 +1,21 @@
+﻿
+function show(elId, target) {
+
+    $(elId).css('height', '100%');
+    $(target).hide();
+    var ParenNode = $(target).parent('.learn-moree')[0];
+    $(ParenNode).find('.min').show();
+    //$(elId).css('background-color', '#0b5b72');
+    
+};
+function hide(elId, target) {
+    $(elId).css('height', '70px');
+    $(target).hide();
+    var ParenNode = $(target).parent('.learn-moree')[0];
+    $(ParenNode).find('.max').show();
+    
+};
+
+$(function () {
+
+});
