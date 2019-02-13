ec5b1657f7ddaad5b36e3f9f55b02af0b4170383
Anton Bessolicyn <Anton.Bessolicyn@danfoss.com>
2019-02-13 18:01:46 +0300

    bootstrap new

--- TechApplication/Scripts/SiteScript.js
+++ TechApplication/Scripts/SiteScript.js
@@ -2,8 +2,9 @@
 function show(elId, target) {
 
     $(elId).css('height', '100%');
+    $(elId).css('width', '100%');
     $(target).hide();
-    var ParenNode = $(target).parent('.learn-moree')[0];
+    var ParenNode = $(target).parent('.card-body')[0];
     $(ParenNode).find('.min').show();
     //$(elId).css('background-color', '#0b5b72');
     
@@ -11,7 +12,7 @@ function show(elId, target) {
 function hide(elId, target) {
     $(elId).css('height', '90px');
     $(target).hide();
-    var ParenNode = $(target).parent('.learn-moree')[0];
+    var ParenNode = $(target).parent('.card-body')[0];
     $(ParenNode).find('.max').show();
     
 };
