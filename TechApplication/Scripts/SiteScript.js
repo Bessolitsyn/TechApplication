387becbe2b499487a4e91ac993c076e66220a9e1
Bessolitsyn <42439921+Bessolitsyn@users.noreply.github.com>
2019-02-28 01:14:38 +0300

    +

--- TechApplication/Scripts/SiteScript.js
+++ TechApplication/Scripts/SiteScript.js
@@ -2,29 +2,44 @@
 function show(elId, target) {
 
     //$('#' + elId).removeClass('height90px');
-    $('.' + elId).removeClass('col-md-3');
-    $('.' + elId).addClass('col-md-12');
+    //$('.' + elId).removeClass('col-md-3');
+    //$('.' + elId).addClass('col-md-12');
     $('#' + elId).show()
     $('.image' + elId).hide();
     $(target).hide();
     var ParenNode = $(target).parent('.card-body .learn-moree')[0];
     $(ParenNode).find('.min').show();
+    $('.' + elId).removeClass('card-img-overlay');
+    //$('.card-img-overlay').removeClass('card-img-overlay')
     //$(elId).css('background-color', '#0b5b72');
     
 };
 function hide(elId, target) {
     //$('#' + elId).addClass('height90px');
-    $('.' + elId).removeClass('col-md-12');
-    $('.' + elId).addClass('col-md-3');
+    //$('.' + elId).removeClass('col-md-12');
+    //$('.' + elId).addClass('col-md-3');
     $('#' + elId).hide()
     $('.image' + elId).show();
-
     $(target).hide();
     var ParenNode = $(target).parent('.card-body .learn-moree')[0];
     $(ParenNode).find('.max').show();
-    
+    $('.' + elId).addClass('card-img-overlay')
 };
 
 $(function () {
+    $('.link').hover(
+        function () {
+        $(this).next().show(); // получаем следующий за данным .link элемент (т.е. span)
+        },
+        function () {
+            $(this).next().hide();
+        }
+    );
+    $('.imagep4').hover(
+        function () {
+            $(this).hide();
+            alert(this);
+        }
+    );
 
 });
